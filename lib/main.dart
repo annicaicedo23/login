5ffc83788f815ed47fe634912b687a28fe5fc0bc
annicaicedo23 <annicaicedo23@gmail.com>
2026-05-25 21:55:32 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'login_screen.dart';
+import 'register_screen.dart';
+import 'home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Configuración de las rutas para navegar las pantallas
+    final GoRouter router = GoRouter(
+      initialLocation: '/login',  
+      routes: [
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          path: '/register',
+          builder: (context, state) => const RegisterScreen(),
+        ),
+        GoRoute(
+          path: '/home',
+          builder: (context, state) => const HomeScreen(),
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
+      title: 'UniRoute Assistant',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      routerConfig: router, // Aquí le decimos a Flutter que use GoRouter
+    );
+  }
+}
